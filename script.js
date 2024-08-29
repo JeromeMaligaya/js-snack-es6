7be5ce26024a2dd7b1cd6864f62f523fd1fd83ff
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-29 17:48:13 +0200

    completed snack 1

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-//Check link
-console.log('JS OK')
+// 1.recupero l'elemento dal Dom
+
+//2.raccolta dati
+const names = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+// 3.lavorazione dati
+
+const invetedList = names.map((guestName, i) =>({
+    // nome del tavolo
+    tableName: 'VIP',
+    // nome invitato
+    guestName,
+    // posto occupato
+    place : i + 1,
+}))
+
+// 4.generazione output
+console.table(invetedList)
+
+
+
+
+
+
+
+
+
+
 
 // 1.recupero l'elemento dal Dom
 
-
 //2.raccolta dati
-    
+
 // 3.lavorazione dati
-    
 
 // 4.generazione output
