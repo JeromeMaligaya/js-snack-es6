668bd8719f7a4fc49ac25099f2d7034aec996d69
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-02 22:40:09 +0200

    snack 3, 4

--- script.js
+++ script.js
@@ -55,3 +55,67 @@ console.log('grades > 70 && id > 120',studs)
 */
 
 //esercizio finito
+
+// -------------------- snack 3
+/*Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal*/
+
+const getRandomNumber = (min = 1, max = 100) => Math.floor(Math.random() * (max - min)) + min;
+
+const bikes = [
+    {name:'bianchi'},
+    {name:'colnago'},
+    {name:'pinarello'},
+    {name:'wilier'},
+    {name:'daccordi'},
+    {name:'olmo'},
+    {name:'cinelli'},
+    {name:'cipollini'},
+]
+
+//assegno il peso per ogni bici
+for(bike of bikes){
+    bike.weight = getRandomNumber(10, 30);
+}
+console.table('bikes', bikes)
+
+let bikeLighter = bikes[0];
+//cerco la bici piu leggera
+bikes.forEach( bike =>{
+    const {weight} = bike;
+    (weight < bikeLighter.weight) ? bikeLighter = bike : bikeLighter;
+});
+// stampo in console la bici piu leggera
+console.log(`la bici più leggera è: ${bikeLighter.name.toUpperCase()} con il peso di ${bikeLighter.weight}`)
+
+// -------------------- snack 4
+/*Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.§*/
+
+const teams = [
+    {team:'ac milan', points: 0, fouls: 0},
+    {team:'as roma', points: 0, fouls: 0},
+    {team:'ss lazio', points: 0, fouls: 0},
+    {team:'fc internazionale', points: 0, fouls: 0},
+    {team:'juventus', points: 0, fouls: 0},
+]
+
+//assegno i punti e i fali per ogni squadra
+teams.forEach( team => {
+    team.points = getRandomNumber(30, 114);
+    team.fouls = getRandomNumber(20, 70);
+})
+
+console.log(teams)
+
+// nuovo array con punti e falli
+const pointsAndFouls = teams.map( element => {
+    const {points, fouls} = element;
+    return {points, fouls}
+})
+// stampo in console il nuovo array
+console.log(pointsAndFouls)
